fae990aa0b7b552d82f50a005d30fba5c7bbea2b
Lydia <lydia.pflug@web.de>
2016-05-06 15:05:17 +0200

    1:N

--- PM_02/src/Aufgabe01/JUnit/JUnitEinsZuN.java
+++ PM_02/src/Aufgabe01/JUnit/JUnitEinsZuN.java
@@ -8,27 +8,30 @@ import org.junit.Before;
 import org.junit.Test;
 
 import Aufgabe01.AdresseNormal;
-import Aufgabe01.PartnerNormal;
+import Aufgabe01.PartnerMulti;
 
 public class JUnitEinsZuN {
+	
+	public PartnerMulti p1;
+	public PartnerMulti p2;
+	public AdresseNormal a1;
+	public AdresseNormal a2;
 
 	@Before
 	public void setUp() {
 		
-//		PartnerMulti p1 = new PartnerMulti("Meyer", "Ingo", new Date(1999, 05, 12));
-		this.p1 = new PartnerNormal("Mueller", "Kai", new Date(1988, 8, 8));
-		this.p2 = new PartnerNormal("Schmidt", "Marion", new Date(1977, 1, 1));
+		this.p1 = new PartnerMulti("Meyer", "Ingo", new Date(1999, 05, 12));
+		this.p2 = new PartnerMulti("Bäcker", "Gustav", new Date(1979, 04,23));
 		this.a1 = new AdresseNormal("Bruecke", 2, 11111, "Berghausen", "Dt");
 		this.a2 = new AdresseNormal("Muelleimer", 5, 22222, "Wiesendorf", "Dt");
-//		AdresseMulti a3 = new AdresseMulti("Am Weg", 3, 33333, "Bergdorf", "Dt");
-		p1.setAdresse(a1);
-		p2.setAdresse(a2);
+		p1.addAdresse(a1);
+		p1.addAdresse(a2);
 		a1.setPartner(p1);
 		a2.setPartner(p2);
 	}
 
 	@Test
-	public void test() {
+	public void test_() {
 		fail("Not yet implemented");
 	}
 	
