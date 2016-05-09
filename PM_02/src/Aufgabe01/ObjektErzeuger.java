cfeccb1d347b87fc498c5bb460fd866031c2657e
Lydia <lydia.pflug@web.de>
2016-05-09 11:12:18 +0200

    ObjektErzeuger erweitert

--- PM_02/src/Aufgabe01/ObjektErzeuger.java
+++ PM_02/src/Aufgabe01/ObjektErzeuger.java
@@ -2,7 +2,14 @@ package Aufgabe01;
 
 public class ObjektErzeuger {
 
-	public static void erzeugen(String name, String vorname, int jahr, int monat, int tag, String strasse, int hausnummer, int plz, String ort, String land)
+	public static Partner erzeugen(String name, String vorname, int jahr, int monat, int tag, String strasse, int hausnummer, int plz, String ort, String land) {
 	
+	PartnerNormal p = new PartnerNormal(name, vorname, jahr, monat, tag);
+	AdresseNormal a = new AdresseNormal(strasse, hausnummer, plz, ort, land);
+	
+	p.setAdresse(a);
+	
+	return p;
 
+	}
 }
