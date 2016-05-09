eff9a641eb8278a21f37970c1ff053e0e4ae00c0
Lydia <lydia.pflug@web.de>
2016-05-09 10:37:20 +0200

    ObjektErzeuger erstellt

---
+++ PM_02/src/Aufgabe01/ObjektErzeuger.java
@@ -0,0 +1,8 @@
+package Aufgabe01;
+
+public class ObjektErzeuger {
+
+	public static void erzeugen(String name, String vorname, int jahr, int monat, int tag, String strasse, int hausnummer, int plz, String ort, String land)
+	
+
+}
