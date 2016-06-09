631b46345c5b5d661b49ce98aae772ca43a7420a
Chris Thiele <chris.thiele@haw-hamburg.de>
2016-06-09 20:19:07 +0200

    Aufgabe 5 ist noch nicht ganz fertig

--- src/Aufgabe052/A03/TestDayStream.java
+++ src/Aufgabe052/A03/TestDayStream.java
@@ -1,5 +1,11 @@
 package Aufgabe052.A03;
 
+import Aufgabe042.PredicateUtility;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.function.Predicate;
+
 /**
  * @author: Chris Thiele
  * @author: Lydia Pflug
@@ -8,4 +14,22 @@ package Aufgabe052.A03;
 
 public class TestDayStream {
 
+    public static void main(String args[]){
+        LocalDate startDatumAufgabeVier = LocalDate.of(2000, 1, 1);
+        LocalDate startDatumAufgabeFuenf = LocalDate.of(2000, 1, 1);
+        Predicate<LocalDate> aufgabe5Predicate;
+
+        aufgabe5Predicate = PredicateUtility.andAll(d -> d.getDayOfMonth() == ) // To-Do
+
+
+        // Ausgabe alle Freitag mit 13.
+        DayStream.erstellenDayStream(startDatumAufgabeVier, 5840)
+                .filter(d -> d.getDayOfWeek() == DayOfWeek.FRIDAY)
+                .filter(d -> d.getDayOfMonth() == 13)
+                .forEach(System.out::println);
+
+        // Ausgabe Aufgabe 05
+        DayStream.erstellenDayStream(startDatumAufgabeFuenf, 5840)
+                .filter()
+    }
 }
